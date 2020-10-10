5674ea856e904521646f3885ec52e0b35806f7d5
megancx <megancxiao@gmail.com>
2020-10-10 18:25:47 -0400

    fix

--- client/screens/ProfileScreen.vue
+++ client/screens/ProfileScreen.vue
@@ -34,13 +34,8 @@
 export default {
   
 }
-$(function () {
-  "use strict";
-  $(".pCard_add").click(function () {
-    $(".pCard_card").toggleClass("pCard_on");
-    $(".pCard_add i").toggleClass("fa-minus");
-  });
-});
+
+
 
 </script>
 
