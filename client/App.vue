f67a93a0d62d32e99388438ed651be7d1d9c219c
Andy Wang <andynmnwang@gmail.com>
2020-10-10 14:31:20 -0500

    add navigation

--- client/App.vue
+++ client/App.vue
@@ -1,17 +1,86 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">My Vue Native App</text>
-  </view>
+  <app-navigator></app-navigator>
 </template>
 
+<script>
+/**
+ * React libraries 
+ */
+import * as React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+/**
+ * Navigation libraries
+ */
+import { createAppContainer, createBottomTabNavigator, createStackNavigator } from 'vue-native-router';
+
+/**
+ * Import screens
+ */
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+const AuthTabNavigator = createBottomTabNavigator(
+  {
+    Login: {
+      screen: LoginScreen,
+      navigationOptions: {
+        title: 'Login',
+        tabBarIcon: () => {
+          return <Icon name="sign-in" size={25} />;
+        }
+      }
+    },
+    Signup: {
+      screen: SignupScreen,
+      navigationOptions: {
+        title: 'Signup',
+        tabBarIcon: () => {
+          return <Icon name="edit" size={25} />;
+        }
+      }
+    }
+  }, {
+    initialRouteName: 'Login'
+  }
+)
+
+const BottomTabNavigator = createBottomTabNavigator(
+  {
+    Profile: ProfileScreen
+  }
+);
+
+const StackNavigator = createStackNavigator(
+  {
+    Auth: {
+      screen: AuthTabNavigator,
+      navigationOptions: {
+        headerStyle: {
+          backgroundColor: '#000'
+        }
+      }
+    },
+    Tabs: {
+      screen: BottomTabNavigator,
+      navigationOptions: {
+        headerStyle: {
+          backgroundColor: '#000'
+        }
+      }
+    },
+  }, {
+    initialRouteName: 'Auth'
+  }
+);
+
+const AppNavigator = createAppContainer(StackNavigator);
+
+export default {
+  components: { AppNavigator }
+}
+</script>
+
 <style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
 </style>
