fa3e9d0dc8d4b6ea343c7b096d8556c8c373785d
Andy Wang <andynmnwang@gmail.com>
2020-10-10 14:31:14 -0500

    add default screens

---
+++ client/screens/ProfileScreen.vue
@@ -0,0 +1,11 @@
+<template>
+  <text> Profile Screen </text>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/screens/SignupScreen.vue
@@ -0,0 +1,11 @@
+<template>
+  <text> Signup Screen </text>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
