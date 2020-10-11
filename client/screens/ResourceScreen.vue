d112bd8fd321e428e8094257ae56f2f10f5e16a6
Andy Wang <andynmnwang@gmail.com>
2020-10-11 11:16:18 -0500

    add kangaroo question image

--- client/screens/ResourceScreen.vue
+++ client/screens/ResourceScreen.vue
@@ -1,8 +1,8 @@
 <template>
   <nb-container class="outer">
     <image
-      :style="{width: '100%', height: '100%', position: 'absolute', top: -100}"
-      :source="require('../assets/announcement.png')" />
+      :style="{width: '100%', height: '60%', position: 'absolute', top: -100}"
+      :source="require('../assets/question.png')" />
     <nb-container class="container">
     <nb-content>
        <nb-list>
