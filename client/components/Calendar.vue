71e0741f6670def2a90a01f3b29ce017279e0e57
Thomas Yeum <yeum.thomas@gmail.com>
2020-10-10 23:33:30 -0600

    iterate through months

--- client/components/Calendar.vue
+++ client/components/Calendar.vue
@@ -1,13 +1,15 @@
 <template>
-    <nb-grid>
-      <nb-col :style="{ backgroundColor: '#635DB7' }"></nb-col>
-      <nb-col :style="{ backgroundColor: '#00CE9F' }"></nb-col>
-      <nb-col :style="{ backgroundColor: '#635DB7' }"></nb-col>
-      <nb-col :style="{ backgroundColor: '#00CE9F' }"></nb-col>
-      <nb-col :style="{ backgroundColor: '#635DB7' }"></nb-col>
-      <nb-col :style="{ backgroundColor: '#00CE9F' }"></nb-col>
-      <nb-col :style="{ backgroundColor: '#635DB7' }"></nb-col>
-    </nb-grid>
+    <nb-container>
+        <nb-grid>
+        <nb-col :style="{ backgroundColor: '#635DB7' }"></nb-col>
+        <nb-col :style="{ backgroundColor: '#00CE9F' }"></nb-col>
+        <nb-col :style="{ backgroundColor: '#635DB7' }"></nb-col>
+        <nb-col :style="{ backgroundColor: '#00CE9F' }"></nb-col>
+        <nb-col :style="{ backgroundColor: '#635DB7' }"></nb-col>
+        <nb-col :style="{ backgroundColor: '#00CE9F' }"></nb-col>
+        <nb-col :style="{ backgroundColor: '#635DB7' }"></nb-col>
+        </nb-grid>
+    </nb-container>
 </template>
 <script>
 export default {
