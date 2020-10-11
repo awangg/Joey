f9fd76ad07bad1d84bd276d3423dd0da7d5c9eb1
megancx <megancxiao@gmail.com>
2020-10-10 22:47:15 -0400

    fonts

--- client/App.vue
+++ client/App.vue
@@ -96,3 +96,17 @@ export default {
   components: { AppNavigator }
 }
 </script>
+
+<style>
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+</style>
