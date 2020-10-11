1179147cbe1f0403d1aba8f9cb01fb6cc3472bfd
megancx <megancxiao@gmail.com>
2020-10-10 21:45:56 -0400

    profile update

--- client/screens/ProfileScreen.vue
+++ client/screens/ProfileScreen.vue
@@ -1,18 +1,94 @@
 <template>
-  
-  
+  <nb-container class="outer">
+    <image
+        :style="{width: '100%', height: '100%', position: 'absolute'}"
+        :source="require('../assets/authbg.png')" />
+      <nb-content padder>
+        <nb-card>
+          <nb-card-item cardBody>
+              <image :source="cardImage" class="card-item-image" :style="stylesObj.cardItemImage"/>
+          </nb-card-item>
+
+          <nb-body>
+            <nb-text class=name>Duncaroo Roo</nb-text>
+          </nb-body>
+
+          <nb-grid>
+            <nb-row class=row>
+              <nb-col class=col><nb-text class=cat>Events</nb-text></nb-col>
+              <nb-col class=col><nb-text class=cat>Points</nb-text></nb-col>
+              <nb-col class=col><nb-text class=cat>Rank</nb-text></nb-col>
+            </nb-row>
+            
+            <nb-row class=row>
+              <nb-col class=col><nb-text class=num>5</nb-text></nb-col>
+              <nb-col class=col><nb-text class=num>100</nb-text></nb-col>
+              <nb-col class=col><nb-text class=num>1</nb-text></nb-col>
+            </nb-row>
+
+          </nb-grid>
+
+        </nb-card>
+      </nb-content>
+
+    
+      
+
+  </nb-container>
 </template>
 
 <script>
-export default {
-  
-}
+  import cardImage from "../assets/circle.png";
+  export default {
+    data: function() {
+      return {
+        cardImage,
+        stylesObj: {
+          cardItemImage: {
+            resizeMode: "cover"
+          }
+        }
+      };
+    }
+  };
 
 
 
 </script>
 
 <style scoped>
+.outer {
+  flex: 1;
+  align-items: center;
+}
 
+.name {
+  bottom: 100;
+  font-size: 40px;
+}
+
+.col {
+  align-items: center;
+  bottom: 20;
+}
+
+.row{
+  bottom: 10%;
+}
+
+.cat{
+  font-size: 25px;
+  color: gray;
+}
+
+.num{
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.card-item-image {
+  left: -2%;
+  top: -20%;
+}
 
 </style>
