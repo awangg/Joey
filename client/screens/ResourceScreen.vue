865661ced7202311c9011bab9121f3a8179f5ee7
megancx <megancxiao@gmail.com>
2020-10-11 01:46:08 -0400

    resources

--- client/screens/ResourceScreen.vue
+++ client/screens/ResourceScreen.vue
@@ -6,7 +6,7 @@
     <nb-container class="container">
     <nb-content>
        <nb-list>
-        <nb-list-item selected>
+        <nb-list-item :on-press="onPressFB">
           <nb-left>
             <nb-text>Duncan Facebook Page</nb-text>
           </nb-left>
@@ -14,7 +14,7 @@
             <nb-icon name="arrow-forward" />
           </nb-right>
         </nb-list-item>
-        <nb-list-item>
+        <nb-list-item :on-press="onPressWeb">
           <nb-left>
             <nb-text>Duncan Website</nb-text>
           </nb-left>
@@ -22,7 +22,7 @@
             <nb-icon name="arrow-forward" />
           </nb-right>
         </nb-list-item>
-        <nb-list-item>
+        <nb-list-item :on-press="onPressAnon">
           <nb-left>
             <nb-text>Anonymous Feedback Form</nb-text>
           </nb-left>
@@ -30,7 +30,7 @@
             <nb-icon name="arrow-forward" />
           </nb-right>
         </nb-list-item>
-        <nb-list-item>
+        <nb-list-item :on-press="onPressPAA">
           <nb-left>
             <nb-text>PAA Question Form</nb-text>
           </nb-left>
@@ -47,8 +47,24 @@
 
 <script>
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {
+  Linking
+} from 'react-native';
 export default {
-  
+   methods: {
+        onPressFB: function() {
+            Linking.openURL("https://www.facebook.com/groups/201995963184811");
+        },
+        onPressWeb: function() {
+            Linking.openURL("https://duncan.rice.edu/");
+        },
+        onPressAnon: function() {
+            Linking.openURL("https://duncan.rice.edu/anonymous-feedback-form/");
+        },
+        onPressPAA: function() {
+            Linking.openURL("https://docs.google.com/forms/d/1iK2H8cst44XZ3uIAArOJVJr1gtgYViV9Ao-5XxAc5GM/viewform?edit_requested=true");
+        },
+    }
 }
 </script>
 
