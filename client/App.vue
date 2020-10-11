209c93eb6adddc5d9c6ebd4070a9c11022ca07af
Sarah Han <swh3@rice.edu>
2020-10-11 00:15:03 -0500

    white border

--- client/App.vue
+++ client/App.vue
@@ -158,7 +158,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color:white;
   display: flex;
   align-items: center;
   justify-content: center;
