c45ec9160fee98fefdf5979f912d4f07df18862e
Thomas Yeum <yeum.thomas@gmail.com>
2020-10-10 23:09:27 -0600

    Got months given real time

--- client/screens/CalendarScreen.vue
+++ client/screens/CalendarScreen.vue
@@ -14,6 +14,9 @@
         <nb-button :on-press="goNext">
           <text>next</text>
         </nb-button>
+        <nb-button :on-press="getMonth">
+          <text>MONTHMONTH</text>
+        </nb-button>
       </nb-right>
     </nb-header>
           <nb-grid>
@@ -22,6 +25,9 @@
               <nb-col :style="{ backgroundColor: '#00CE9F' }"></nb-col>
             </nb-row>
           </nb-grid>
+  </nb-container>
+  <nb-container>
+      <Calendar />
   </nb-container>
   </view>
 </template>
@@ -33,16 +39,32 @@ export default {
   props: {
     navigation: { type: Object } 
   },
-  components:
-  {
-    Calendar
+  components: { Calendar },
+  data() {
+    return {
+      month: '',
+      months: [
+            'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
+          ]
+    }
+  },
+  created() {
+    this.getMonth()
   },
   methods: {
         goBack: function() {
             alert('MWAHAHAHA GO BACK')
+            this.month = 
         },
         goNext: function() {
             alert('MWAHAHAHA GO NEXT')
+        },
+        getMonth: function() {
+          var today = new Date()
+          var dd = String(today.getDate()).padStart(2, '0') // get the DAY
+          var mm = String(today.getMonth() + 1).padStart(2, '0')
+          alert(this.months[mm - 1])
+          return this.months[mm - 1]
         }
     }
 }
